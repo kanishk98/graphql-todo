042a30b5ddb8e286693aba16c9f1f6b0093fc594
kanishk98 <kanishk.kakar@gmail.com>
2018-12-21 19:36:00 +0530

    Move TodoInput and TodoList to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap';
+import { Query } from 'react-apollo';
 import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+import { todos } from '../graphql';
+import { client } from '../index';
 import Constants from '../Constants';
 
 export default class extends React.Component {
@@ -8,11 +13,42 @@ export default class extends React.Component {
 		super(props);
 		this.state = {
 			list: [],
-		}
+			text: null
+		};
 	}
 
+	_onChangeText = ({ target }) => {
+		this.setState({ text: target.value });
+	};
+
+	_onSubmitTodo = () => {
+		if (!!this.state.text) {
+			this.sendToDB();
+		}
+	};
+
+	sendToDB = () => {
+		// add API post logic here
+		console.log('sending to db');
+	};
+
 	componentDidMount() {
-		// TODO: API call for fetching items
+		client
+			.query({
+				query: todos, options: {
+					context: {
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					}
+				}
+			})
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.log(err);
+			});
 	}
 
 	render() {
@@ -20,6 +56,19 @@ export default class extends React.Component {
 			// redirect user to login page
 			return <Redirect to="/login" />;
 		}
-		return <TodoInput list={this.state.list} />;
+		return (
+			<>
+				<InputGroup>
+					<Input
+						placeholder={'What are you up to?'}
+						onChange={e => this._onChangeText(e)}
+					/>
+					<InputGroupAddon addonType="append">
+						<Button onClick={this._onSubmitTodo}>Add to-do</Button>
+					</InputGroupAddon>
+				</InputGroup>
+				<br />
+			</>
+		);
 	}
 }
